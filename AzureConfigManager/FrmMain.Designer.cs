8920d0e879478759b1bd41bd90e4b938648efe56
David Hoare <d.hoare@marketinvoice.com>
2017-03-01 11:47:02 +0000

    More

---
+++ AzureConfigManager/FrmMain.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace AzureConfigManager
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnLoadCredentials = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lstApps = new System.Windows.Forms.ListBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.appSettingsGrid = new System.Windows.Forms.DataGridView();
+            this.Key = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.connectionStringsGrid = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnCommit = new System.Windows.Forms.Button();
+            this.comboSubscription = new System.Windows.Forms.ComboBox();
+            this.ConnectionName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ConnectionString = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IsSql = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.appSettingsGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.connectionStringsGrid)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoadCredentials
+            // 
+            this.btnLoadCredentials.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoadCredentials.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadCredentials.Location = new System.Drawing.Point(12, 39);
+            this.btnLoadCredentials.Name = "btnLoadCredentials";
+            this.btnLoadCredentials.Size = new System.Drawing.Size(610, 29);
+            this.btnLoadCredentials.TabIndex = 0;
+            this.btnLoadCredentials.Text = "Connect";
+            this.btnLoadCredentials.UseVisualStyleBackColor = true;
+            this.btnLoadCredentials.Click += new System.EventHandler(this.btnLoadCredentials_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 74);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lstApps);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(610, 422);
+            this.splitContainer1.SplitterDistance = 125;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // lstApps
+            // 
+            this.lstApps.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstApps.FormattingEnabled = true;
+            this.lstApps.Location = new System.Drawing.Point(0, 0);
+            this.lstApps.Name = "lstApps";
+            this.lstApps.Size = new System.Drawing.Size(125, 422);
+            this.lstApps.TabIndex = 0;
+            this.lstApps.SelectedIndexChanged += new System.EventHandler(this.lstApps_SelectedIndexChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.appSettingsGrid);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.connectionStringsGrid);
+            this.splitContainer2.Size = new System.Drawing.Size(481, 422);
+            this.splitContainer2.SplitterDistance = 285;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // appSettingsGrid
+            // 
+            this.appSettingsGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.appSettingsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.appSettingsGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Key,
+            this.Value});
+            this.appSettingsGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.appSettingsGrid.Location = new System.Drawing.Point(0, 0);
+            this.appSettingsGrid.Name = "appSettingsGrid";
+            this.appSettingsGrid.Size = new System.Drawing.Size(481, 285);
+            this.appSettingsGrid.TabIndex = 0;
+            // 
+            // Key
+            // 
+            this.Key.HeaderText = "Key";
+            this.Key.Name = "Key";
+            this.Key.Width = 50;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.Width = 59;
+            // 
+            // connectionStringsGrid
+            // 
+            this.connectionStringsGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.connectionStringsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.connectionStringsGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ConnectionName,
+            this.ConnectionString,
+            this.IsSql});
+            this.connectionStringsGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.connectionStringsGrid.Location = new System.Drawing.Point(0, 0);
+            this.connectionStringsGrid.Name = "connectionStringsGrid";
+            this.connectionStringsGrid.Size = new System.Drawing.Size(481, 133);
+            this.connectionStringsGrid.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 531);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(634, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(39, 17);
+            this.statusLabel.Text = "Ready";
+            // 
+            // btnCommit
+            // 
+            this.btnCommit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCommit.Location = new System.Drawing.Point(12, 502);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(610, 23);
+            this.btnCommit.TabIndex = 3;
+            this.btnCommit.Text = "Commit";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
+            // comboSubscription
+            // 
+            this.comboSubscription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboSubscription.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboSubscription.FormattingEnabled = true;
+            this.comboSubscription.Items.AddRange(new object[] {
+            "Codat Production=8788e380-a3ab-4104-8ddb-951356d22460",
+            "Codat Bizspark (David)=3cc41c9f-fa1a-42c1-ad39-8961c0ec5267"});
+            this.comboSubscription.Location = new System.Drawing.Point(12, 12);
+            this.comboSubscription.Name = "comboSubscription";
+            this.comboSubscription.Size = new System.Drawing.Size(610, 21);
+            this.comboSubscription.TabIndex = 4;
+            // 
+            // ConnectionName
+            // 
+            this.ConnectionName.HeaderText = "Name";
+            this.ConnectionName.Name = "ConnectionName";
+            this.ConnectionName.Width = 60;
+            // 
+            // ConnectionString
+            // 
+            this.ConnectionString.HeaderText = "ConnectionString";
+            this.ConnectionString.Name = "ConnectionString";
+            this.ConnectionString.Width = 113;
+            // 
+            // IsSql
+            // 
+            this.IsSql.HeaderText = "Is SQL?";
+            this.IsSql.Name = "IsSql";
+            this.IsSql.Width = 51;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 553);
+            this.Controls.Add(this.comboSubscription);
+            this.Controls.Add(this.btnCommit);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btnLoadCredentials);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "Azure Config Viewer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.appSettingsGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.connectionStringsGrid)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadCredentials;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lstApps;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView appSettingsGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Key;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridView connectionStringsGrid;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Button btnCommit;
+        private System.Windows.Forms.ComboBox comboSubscription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ConnectionName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ConnectionString;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn IsSql;
+    }
+}
+
---
+++ AzureConfigManager/FrmMain.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.WindowsAzure.Management.WebSites;
+using Microsoft.WindowsAzure.Management.WebSites.Models;
+
+namespace AzureConfigManager
+{
+    public partial class FrmMain : Form
+    {
+        private WebApp[] _apps;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLoadCredentials_Click(object sender, EventArgs e)
+        {
+            await RefreshData();
+        }
+
+        private async Task RefreshData()
+        {
+            statusLabel.Text = "Connecting to Azure...";
+            DisableButtons();
+            var token = GetAuthorizationHeader();
+            var subscriptionId = comboSubscription.SelectedItem.ToString().Split('=')[1];
+            var credentials = new TokenCloudCredentials(subscriptionId, token);
+            statusLabel.Text = "Fetching data...";
+            using (var client = new WebSiteManagementClient(credentials)) {
+                var spaces = await client.WebSpaces.ListAsync();
+                var sites = (await Task.WhenAll(spaces.Select(async s => await client.WebSpaces.ListWebSitesAsync(s.Name, new WebSiteListParameters())).ToList())).SelectMany(a => a);
+
+                _apps = await Task.WhenAll(sites.Select(async s => await GetWebApp(client, s)).ToList());
+            }
+
+            _apps = _apps.OrderBy(a => a.Name).ToArray();
+            lstApps.Items.Clear();
+            lstApps.Items.AddRange(_apps.Select(a => a.Name).ToArray());
+            statusLabel.Text = $"{_apps.Length} Web Apps loaded";
+
+            EnableButtons();
+        }
+
+        private static async Task<WebApp> GetWebApp(IWebSiteManagementClient client, WebSite site)
+        {
+            var currentConfig = await client.WebSites.GetConfigurationAsync(site.WebSpace, site.Name, CancellationToken.None);
+            var settings = currentConfig.AppSettings.Select(d => new Setting { Key = d.Key, Value = d.Value, IsSql = false }).ToList();
+            var connectionStrings = currentConfig.ConnectionStrings.Select(d =>
+                new Setting
+                {
+                    Key = d.Name,
+                    Value = d.ConnectionString,
+                    IsSql = (d.Type == ConnectionStringType.SqlAzure || d.Type == ConnectionStringType.SqlServer)
+                }).ToList();
+
+            var webApp = new WebApp() {
+                Name = site.Name,
+                WebSpace = site.WebSpace,
+                AppSettings = settings,
+                ConnectionStrings = connectionStrings
+            };
+            return webApp;
+        }
+
+        private static string GetAuthorizationHeader()
+        {
+            AuthenticationResult result = null;
+
+            var context = new AuthenticationContext(string.Format(
+              ConfigurationManager.AppSettings["login"],
+              ConfigurationManager.AppSettings["tenantId"]));
+
+            var thread = new Thread(() => {
+                result = context.AcquireToken(
+                  ConfigurationManager.AppSettings["apiEndpoint"],
+                  ConfigurationManager.AppSettings["clientId"],
+                  new Uri(ConfigurationManager.AppSettings["redirectUri"]));
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Name = "AquireTokenThread";
+            thread.Start();
+            thread.Join();
+
+            if (result == null) {
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+            }
+
+            string token = result.AccessToken;
+            return token;
+        }
+
+        internal class WebApp
+        {
+            public string Name { get; set; }
+            public string WebSpace { get; set; }
+            public List<Setting> AppSettings { get; set; }
+            public List<Setting> ConnectionStrings { get; set; }
+        }
+
+        internal class Setting
+        {
+            public string Key { get; set; }
+            public string Value { get; set; }
+            public bool IsSql { get; set; }
+        }
+
+        private void lstApps_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var name = lstApps.SelectedItem.ToString();
+            var app = _apps.Single(a => a.Name == name);
+
+            PopulateDataGridFromDictionary(appSettingsGrid, app.AppSettings, false);
+
+            PopulateDataGridFromDictionary(connectionStringsGrid, app.ConnectionStrings, true);
+        }
+
+        private static void PopulateDataGridFromDictionary(DataGridView dataGrid, List<Setting> dictionary, bool withSql)
+        {
+            object[][] rows;
+
+            if (withSql)
+            {
+                rows = dictionary.Select(a => new object[] {a.Key, a.Value, a.IsSql}).ToArray();
+            }
+            else
+            {
+                rows = dictionary.Select(a => new object[] { a.Key, a.Value }).ToArray();
+            }
+            
+            dataGrid.Rows.Clear();
+            foreach (var row in rows) {
+                dataGrid.Rows.Add(row);
+            }
+        }
+
+        private static void PopulateDictionaryFromDataGrid(List<Setting> dictionary, DataGridView dataGrid)
+        {
+            dictionary.Clear();
+            foreach (var row in dataGrid.Rows) {
+                var rowArray = (row as DataGridViewRow).Cells;
+                if (rowArray[0].Value != null && rowArray[1].Value != null) {
+                    if (rowArray.Count == 3)
+                    {
+                        dictionary.Add(new Setting
+                        {
+                            Key = rowArray[0].Value.ToString(),
+                            Value = rowArray[1].Value.ToString(),
+                            IsSql = (bool) rowArray[2].Value
+                        });
+                    }
+                    else
+                    {
+                        dictionary.Add(new Setting
+                        {
+                            Key = rowArray[0].Value.ToString(),
+                            Value = rowArray[1].Value.ToString(),
+                            IsSql = false
+                        });
+                    }
+                }
+            }
+        }
+
+        private async void btnCommit_Click(object sender, EventArgs e)
+        {
+            DisableButtons();
+            var name = lstApps.SelectedItem.ToString();
+            var app = _apps.Single(a => a.Name == name);
+            PopulateDictionaryFromDataGrid(app.AppSettings, appSettingsGrid);
+            PopulateDictionaryFromDataGrid(app.ConnectionStrings, connectionStringsGrid);
+
+            statusLabel.Text = "Connecting to Azure...";
+            var token = GetAuthorizationHeader();
+            var subscriptionId = comboSubscription.SelectedItem.ToString().Split('=')[1];
+            var credentials = new TokenCloudCredentials(subscriptionId, token);
+            statusLabel.Text = "Setting data...";
+
+            var connectionStrings =
+            app.ConnectionStrings.Select(c => new WebSiteUpdateConfigurationParameters.ConnectionStringInfo
+            {
+                Name = c.Key,
+                ConnectionString = c.Value,
+                Type = c.IsSql ? ConnectionStringType.SqlAzure : ConnectionStringType.Custom
+            }).ToList();
+            
+
+            var newConfig = new WebSiteUpdateConfigurationParameters
+            {
+                ConnectionStrings = connectionStrings,
+                DefaultDocuments = null,
+                HandlerMappings = null,
+                Metadata = null,
+                AppSettings = app.AppSettings.ToDictionary(s => s.Key, s => s.Value)
+            };
+            
+            using (var client = new WebSiteManagementClient(credentials)) {
+                await client.WebSites.UpdateConfigurationAsync(app.WebSpace, app.Name, newConfig);
+            }
+
+            EnableButtons();
+
+            await RefreshData();
+        }
+
+        private void DisableButtons()
+        {
+            btnCommit.Enabled = false;
+            btnLoadCredentials.Enabled = false;
+        }
+
+        private void EnableButtons()
+        {
+            btnCommit.Enabled = true;
+            btnLoadCredentials.Enabled = true;
+        }
+    }
+}
