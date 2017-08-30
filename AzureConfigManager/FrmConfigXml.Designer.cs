b6a69118b95af8d2447e47b9db95da522f3db503
David Hoare <david@codat.io>
2017-08-30 15:04:47 +0100

    Tidy up the Web.config XML viewer

--- AzureConfigManager/FrmConfigXml.Designer.cs
+++ AzureConfigManager/FrmConfigXml.Designer.cs
@@ -32,63 +32,102 @@
             System.Windows.Forms.Label label2;
             this.txtAppSettings = new System.Windows.Forms.TextBox();
             this.txtConnectionStrings = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(9, 9);
+            label1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.Location = new System.Drawing.Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(75, 13);
+            label1.Size = new System.Drawing.Size(96, 11);
             label1.TabIndex = 2;
             label1.Text = "<appSettings>";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(12, 139);
+            label2.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label2.Location = new System.Drawing.Point(3, 0);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(104, 13);
+            label2.Size = new System.Drawing.Size(138, 11);
             label2.TabIndex = 3;
             label2.Text = "<connectionStrings>";
             // 
             // txtAppSettings
             // 
-            this.txtAppSettings.Location = new System.Drawing.Point(12, 26);
+            this.txtAppSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAppSettings.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAppSettings.Location = new System.Drawing.Point(3, 16);
             this.txtAppSettings.Multiline = true;
             this.txtAppSettings.Name = "txtAppSettings";
-            this.txtAppSettings.Size = new System.Drawing.Size(260, 107);
+            this.txtAppSettings.Size = new System.Drawing.Size(509, 175);
             this.txtAppSettings.TabIndex = 0;
             this.txtAppSettings.Click += new System.EventHandler(this.txtAppSettings_Click);
             // 
             // txtConnectionStrings
             // 
-            this.txtConnectionStrings.Location = new System.Drawing.Point(12, 155);
+            this.txtConnectionStrings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtConnectionStrings.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConnectionStrings.Location = new System.Drawing.Point(3, 16);
             this.txtConnectionStrings.Multiline = true;
             this.txtConnectionStrings.Name = "txtConnectionStrings";
-            this.txtConnectionStrings.Size = new System.Drawing.Size(260, 94);
+            this.txtConnectionStrings.Size = new System.Drawing.Size(509, 172);
             this.txtConnectionStrings.TabIndex = 1;
             this.txtConnectionStrings.Click += new System.EventHandler(this.txtConnectionStrings_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtAppSettings);
+            this.splitContainer1.Panel1.Controls.Add(label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtConnectionStrings);
+            this.splitContainer1.Panel2.Controls.Add(label2);
+            this.splitContainer1.Size = new System.Drawing.Size(515, 389);
+            this.splitContainer1.SplitterDistance = 194;
+            this.splitContainer1.TabIndex = 4;
             // 
             // FrmConfigXml
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(label2);
-            this.Controls.Add(label1);
-            this.Controls.Add(this.txtConnectionStrings);
-            this.Controls.Add(this.txtAppSettings);
+            this.ClientSize = new System.Drawing.Size(539, 413);
+            this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmConfigXml";
             this.Text = "Web.config Xml";
             this.Load += new System.EventHandler(this.FrmConfigXml_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -96,5 +135,6 @@
 
         private System.Windows.Forms.TextBox txtAppSettings;
         private System.Windows.Forms.TextBox txtConnectionStrings;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
